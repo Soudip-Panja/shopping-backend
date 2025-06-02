50ce2f682c03766de7ab606b1e81ebfa8f3f3594
Soudip Panja <soudip03panja@gmail.com>
2025-06-02 13:52:21 +0530

    feature: Shopping website backend model.

---
+++ model/product.model.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: [
+        {
+            type: String,
+            enum: ["Men", "Women", "Girl", "Boy", "Accessories"]
+        }
+    ],
+    collectionType: [
+        {
+            type: String,
+            enum: ["Summer Collection", "Accessories Collection", "Trending Shoe Collection", "Glow Up Collection"]
+        }
+    ],
+    price: {
+        type: Number,
+        required: true
+    },
+    size: [
+        {
+            clothSize:{
+                type: String,
+                enum: ["S", "M", "L", "XL", "XXL"]
+            },
+            shoesSize:{
+                type: Number,
+                enum: [6, 7, 8, 9, 10]
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true
+    },
+    tags: {
+        type: String
+    },
+    imageUrl: [
+        {
+            type: String,
+            required: true
+        }
+    ]
+},
+{
+    timestamps: true
+}
+)
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
